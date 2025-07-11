35b0dfbf48021338e79241d6a4d86150128c3d51
jianlongchenn <jianlong@hawaii.edu>
2025-07-10 17:02:16 -1000

    fix: url

--- vocalcollab_frontend/src/app/lib/fetchWithAuth.ts
+++ vocalcollab_frontend/src/app/lib/fetchWithAuth.ts
@@ -17,7 +17,7 @@ export async function fetchWithAuth(input: RequestInfo, init: RequestInit = {}):
   })
 
   if (response.status === 401 && refreshToken) {
-    const refreshRes = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
+    const refreshRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/token/refresh/`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ refresh: refreshToken }),
