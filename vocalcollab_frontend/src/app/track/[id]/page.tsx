aae79820057bb387574cad150799e67751b389ea
jianlongchenn <jianlong@hawaii.edu>
2025-07-10 19:37:04 -1000

    edit url

--- vocalcollab_frontend/src/app/track/[id]/page.tsx
+++ vocalcollab_frontend/src/app/track/[id]/page.tsx
@@ -31,7 +31,7 @@ export default function TrackDetailPage() {
 
   const fetchComments = async () => {
     try {
-      const res = await fetch(`http://127.0.0.1:8000/api/tracks/${id}/comments/`)
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tracks/${id}/comments/`)
       const data = await res.json()
       setComments(data)
     } catch (err) {
@@ -41,7 +41,7 @@ export default function TrackDetailPage() {
 
   useEffect(() => {
     const fetchTrack = async () => {
-      const res = await fetch(`http://127.0.0.1:8000/api/tracks/${id}/`)
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tracks/${id}/`)
       const data = await res.json()
       setTrack(data)
     }
